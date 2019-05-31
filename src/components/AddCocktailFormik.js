79b80d34b5e49dfdd07e40f479ca18fcdbe1ace2
andreeaparauleonte <49757605+andreeaparauleonte@users.noreply.github.com>
2019-05-31 16:12:26 +0200

    Category with useState, useEffect

--- src/components/AddCocktailFormik.js
+++ src/components/AddCocktailFormik.js
@@ -1,7 +1,98 @@
-import React, {Component} from 'react';
+import React, {Component,useState} from 'react';
 import { Formik } from 'formik';
+import {   
+    Link
+  } from 'react-router-dom';
 
-class AddCocktailFormik  extends Component{
+const AddCocktailFormik  = (props) => {
+      
+    const [formular,setFormular] = useState({
+        image : null,
+        name: "andreea's special",
+        ingredient1:"votca",
+        ingredient2:"orange juice",
+        quantity:2
+
+    });
+
+    const handleChange = (e) => {
+        console.log("in handle");
+                setFormular({...formular,
+            [e.target.name] : e.target.value
+        });
+    };
+
+        return (<div>
+            <h1>My Form</h1>
+            <Formik
+              /*initialValues={{ name: "andreea's special", ingredient1: "votca", ingredient2 :"orange juice", quantity: 2 }}*/
+              onSubmit={(values, actions) => {
+                  console.log(formular);
+                    var newCocktail = {strDrink:formular.name, strDrinkThumb:formular.image, idDrink:"1000001"};
+                    props.doAddCocktail(newCocktail);
+                    props.history.goBack();
+                
+              }}
+              onChange = {(e) => {
+                  console.log("changed ");
+                  if(e.target.name ==="pic"){
+                      console.log("pic handleChange");
+                  }
+                  else{
+                    setFormular({...formular,
+                        [e.target.name] : e.target.value
+                    });
+                  }
+                
+              }}
+              render={formikProps=> (
+                  <div className="categoryContainer">
+                  
+                      <form onSubmit={formikProps.handleSubmit}>
+                          <div className="imageDiv">
+                              <input type="file" name="pic" accept="image/*" onChange={(event)=>{
+                                    if (event.target.files && event.target.files[0]) {
+                                        let reader = new FileReader();
+                                        reader.onload = (e) => {
+                                            setFormular({"image":e.target.result});
+                                        };
+                                        reader.readAsDataURL(event.target.files[0]);
+                                    }
+
+                              }} />
+                              <img src={formular.image} alt="select image"/>
+                          </div>
+                          <div className="detailsDiv">
+                              <div className="rowForm">
+                                  <label>Name</label>
+                                  <input type="text" name="name" onChange={handleChange} onBlur={formikProps.handleBlur} value={formular.name} />
+                              </div>
+                              <div className="rowForm">
+                                  <label>Ingredient 1</label>
+                                  <input type="text" name="ingredient1" onChange={handleChange} onBlur={formikProps.handleBlur} value={formular.ingredient1}/>
+                              </div>
+                              <div className="rowForm">
+                                  <label>Ingredient 2</label>
+                                  <input type="text" name="ingredient2" onChange={handleChange} onBlur={formikProps.handleBlur} value={formular.ingredient2}/>
+                              </div>
+                              <div className="rowForm">
+                                  <label>Quantity</label>
+                                  <input type="text" name="quantity" onChange={handleChange} onBlur={formikProps.handleBlur} value={formular.quantity}/>
+                              </div>
+                              <div className="rowForm">
+                                  <button type="submit">Submit</button>
+                              </div>
+                          </div>
+                      </form>
+                      <Link to={props.back}> <button className="backButton" /*onClick={props.history.goBack}*/>Back</button></Link>
+                  </div>
+              )}
+            />
+          </div>)
+}
+
+/**
+ * class AddCocktailFormik  extends Component{
     constructor(props){
         super(props);
         this.state ={
@@ -68,5 +159,6 @@ class AddCocktailFormik  extends Component{
           </div>)
     }
 }
+ */
 
   export default AddCocktailFormik;
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import './Category.css';
 import axios from 'axios';
 import {   
@@ -11,14 +11,63 @@ import Cocktail from './Cocktail';
 import CocktailView from './CocktailView';
 import AddCocktailFormik from './AddCocktailFormik';
 
+const Category = (props) =>  {
+    const [cocktails,setCocktails] = useState([]); 
+
+    useEffect(() => {
+        console.log("in useEffect");
+        let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?' + props.filterType + '=' + props.filterBy;
+        axios.get(url).then(response => {
+            console.log("in axios respo");
+            setCocktails(response.data.drinks);
+            }
+        );
+
+    },[setCocktails]);
+
+    const addCocktailToState = (cocktail) => {
+        setCocktails([...cocktails, cocktail]);
+    }
+
+
+        let categoryid= props.matchCategory.params.id;
+        console.log("in before return category");    
+        return (<Switch>
+                    <Route path={"/"+categoryid + "/add-cocktail"} render={() => 
+                        {return (<AddCocktailFormik doAddCocktail={addCocktailToState} history={props.history} back={"/"+categoryid} />)}}/>     
+                    
+                    <Route exact path={"/" + categoryid+"/:id"} render={(props) => {
+                            let cocktailId = props.match.params.id;                         
+                            let cocktail = cocktails.filter(x=>x.idDrink === cocktailId)[0];
+                            return (<CocktailView {...cocktail} history={props.history} />)
+                        }} />
+                                
+                    <Route path={"/" + categoryid} render={()=> {
+                            let cocktailsComp = cocktails.map((cocktail, index) => 
+                                {return <Cocktail {...cocktail} key={cocktail.idDrink} categoryid={categoryid}/>                        
+                            });
+                            return (<div className="categoryContainer">
+                                        <div className="categoryTitle">{props.name}</div>          
+                                        {cocktailsComp}
+                                        <Link className="AddCocktailButton" to={ "/"+categoryid + "/add-cocktail"} > Add</Link>
+                                    </div>)
+                            }}/>                                      
+         </Switch>       
+        );
+        
+}
+
+/*
 class Category extends Component {
     state = {
         cocktails: []
     }
 
     componentDidMount(){
+        console.log("in componentDidMount");
         let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?' + this.props.filterType + '=' + this.props.filterBy;
         axios.get(url).then(response => {
+            console.log("in axios respo");
             this.setState({cocktails : response.data.drinks })
             }
         );     
@@ -32,10 +81,10 @@ class Category extends Component {
 
     render(){
         let categoryid= this.props.matchCategory.params.id;
-              
+        console.log("in render");    
         return (<Switch>
                     <Route path={"/"+categoryid + "/add-cocktail"} render={() => 
-                        {return (<AddCocktailFormik doAddCocktail={this.addCocktailToState.bind(this)} history={this.props.history} />)}}/>     
+                        {return (<AddCocktailFormik doAddCocktail={this.addCocktailToState.bind(this)} history={this.props.history} back={"/"+categoryid} />)}}/>     
                     
                     <Route exact path={"/" + categoryid+"/:id"} render={(props) => {
                             let cocktailId = props.match.params.id;                         
@@ -57,5 +106,6 @@ class Category extends Component {
         );
         }
 }
+* */
 
 export default Category;
